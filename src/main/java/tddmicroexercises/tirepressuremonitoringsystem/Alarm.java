0cccf6674f7caad1f86737bee02669977a468c36
trikitrok <trikitrok@gmail.com>
2015-03-28 14:13:22 +0100

    Extracted value object for storing the safety range

--- src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
+++ src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
@@ -1,27 +1,41 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
 public class Alarm {
+    private class SafeRange {
+
+        private double lowerBound;
+        private double higherBound;
+
+        public SafeRange(double lowerBound, double higherBound) {
+            this.lowerBound = lowerBound;
+            this.higherBound = higherBound;
+        }
+
+        public boolean isNotSafe(double value) {
+            return value < lowerBound || higherBound < value;
+        }
+
+    }
+
     private final double LowPressureThreshold = 17;
     private final double HighPressureThreshold = 21;
 
     private Sensor sensor;
 
     private boolean alarmOn = false;
+    private SafeRange safeRange;
 
     public Alarm(Sensor sensor) {
         this.sensor = sensor;
+        this.safeRange = new SafeRange(LowPressureThreshold, HighPressureThreshold);
     }
 
     public void check() {
         double pressure = sensor.probePressure();
 
-        if (isPressureOutsideSafeRange(pressure)) {
+        if (safeRange.isNotSafe(pressure)) {
             alarmOn = true;
         }
-    }
-
-    private boolean isPressureOutsideSafeRange(double pressure) {
-        return pressure < LowPressureThreshold || HighPressureThreshold < pressure;
     }
 
     public boolean isAlarmOn() {
